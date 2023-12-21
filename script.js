6d9e325ae396004f6a17d087430f997b7dd5fa3d
Jamie Wilson <howemanning@gmail.com>
2023-12-21 15:08:35 +1300

    Add form validation for all inputs

--- script.js
+++ script.js
@@ -20,14 +20,36 @@ document.addEventListener("DOMContentLoaded", function () {
   function validateForm() {
     console.log("Running validateForm");
     var inputs = document.querySelectorAll("#myForm .input-container input");
+
     inputs.forEach((input) => {
+      let errorElementId = input.name + "-error"; // Construct the error element ID
+      let errorElement = document.getElementById(errorElementId);
+
       if (input.value.trim() === "") {
         console.log("Empty input found", input);
         input.parentElement.classList.add("input-container-error");
+        if (errorElement) {
+          errorElement.style.display = "block";
+        }
+      } else if (input.type === "email" && !validateEmail(input.value)) {
+        console.log("Invalid email found", input);
+        input.parentElement.classList.add("input-container-error");
+        if (errorElement) {
+          errorElement.textContent = "Looks like this is not an email"; // Update error message for email
+          errorElement.style.display = "block";
+        }
       } else {
         console.log("Valid input found", input);
         input.parentElement.classList.remove("input-container-error");
+        if (errorElement) {
+          errorElement.style.display = "none";
+        }
       }
     });
+  }
+
+  function validateEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Simple email validation regex
+    return emailRegex.test(email);
   }
 });
